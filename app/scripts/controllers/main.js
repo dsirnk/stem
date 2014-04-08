8057846ece3a762b8db0f36c3e9ce614c17f6021
Ronak Desai <rdesai@klick.com>
2014-04-07 22:33:20 -0400

    0.0.5: Angular factory for genome added
    #todo:
    + update status on change or at regular interval
    - highlight user on status change
    - implement aync for loop
    - store images in localstorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,28 +3,14 @@
 
 angular
   .module('zenomeApp')
-  .controller('MainCtrl', function ($scope, $http, $filter) {
-    $scope.alerts = $scope.users = [];
+  .controller('MainCtrl', function ($scope, $http, $filter, Genome) {
     /*==========  Initialize scope variables  ==========*/
+    $scope.alerts = [];
     $scope.userList = angular.fromJson(localStorage.userList || '[]');
 
-    var genome = 'http://genome.klick.com',
-        /*==========  Get Data from API  ==========*/
-        GenomeAPI = function (param, callback) {
-          var config = { format: 'json', callback: 'JSON_CALLBACK' };
-          $http
-            .jsonp(genome + '/api/User', {params: angular.extend(config, param)})
-            // .get( genome + '/elements/javascripts/data/enabledusers/' )
-            .success(callback)
-            .error(function() {
-              $scope.alerts.push({
-                type: 'danger',
-                msg: 'Couldn\'t connect to the API.\n' +
-                  'Please ensure you are connected to the internet and' +
-                  'Logged into http://geome.klick.com'
-              });
-            });
-        };
+    /*==========  Get List of Users  ==========*/
+    $scope.user = Genome.Users.get();
+    // $scope.user = ComboUsersData;
 
     /*==========  Watch userList for changes to sync localStorage  ==========*/
     $scope
@@ -34,26 +20,15 @@ angular
         }
       },true);
 
-    /*==========  Get List of Users  ==========*/
-    new GenomeAPI(
-      { ForAutocompleter: true, ForGrid: true },
-      function(r) { $scope.users = r.Entries; }
-    );
-    // $scope.users = ComboUsersData;
     $scope
       .userSelected = function ($item) {
+        var user = { UserID: $item.UserID };
         /*==========  Avoid Duplicate Entires in userList  ==========*/
-        if (!$filter('filter')($scope.userList, {UserID: $item.UserID}).length) {
-          /*==========  Get User Data  ==========*/
-          new GenomeAPI(
-            { userID: $item.UserID },
-            function (r) {
-              $item = r.Entries[0];
-              $item.PhotoPath = genome + $item.PhotoPath;
-              /*==========  Add to userList  ==========*/
-              $scope.userList.push($item);
-            }
-          );
+        if (!$filter('filter')($scope.userList, user).length) {
+          Genome.User.get(user)
+            .$promise.then(function(r) {
+              $scope.userList.push(r);
+            });
         }
         $scope.userSelect = null;
       };
@@ -80,6 +55,33 @@ angular
       .userUpdate = setInterval(function() {
         // $scope.userList
     }, 15000);
+  /*==========  User API Interaction  ==========*/
+  .factory('Genome', function($resource) {
+    var genome = 'http://genome.klick.com',
+        genomeAPI = { format: 'json', callback: 'JSON_CALLBACK' },
+        counter = 0;
+
+    return {
+      Users: $resource(
+        genome + '/api/User/',
+        { ForAutocompleter: true, ForGrid: true },
+        { get: { method: 'JSONP', params: genomeAPI } }
+      ),
+      User: $resource(
+        genome + '/api/User/',
+        { UserID: '@UserID' },
+        { get: { method: 'JSONP', params: genomeAPI,
+            transformResponse: function(r) {
+              r.Entries[0].PhotoPath = genome + r.Entries[0].PhotoPath;
+              if (r.Entries[0].UserID === 4806) {
+                r.Entries[0].KeyscanStatus = ('IN' + ++counter);
+              }
+              return r.Entries[0];
+            }
+          }
+        }
+      )
+    };
   })
   // /*==========  Store images offline  ==========*/
   // .directive('onimageload', function () {
